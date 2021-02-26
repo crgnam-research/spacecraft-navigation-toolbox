9497ebd00ca8b7a6780ddc683089d8a6668f4e86
Chris Gnam <crgnam@buffalo.edu>
2021-02-25 20:33:16 -0700

    added data and basic framework to the first ukf demo for this toolbox

---
+++ demos/ukf_class_orbit_determination/ukf_models/ukf_measurement_model.m
@@ -0,0 +1,30 @@
+function [predicted_measurement] = ukf_measurement_model(~,X,earth_angularRate,gs1,gs2,gs3,avails)
+    
+    r = X(1:3);
+    v = X(4:6);
+    
+    % Calculate predicted range measurements:
+    [ray1,range1] = normc(r-gs1.position_frame);
+    [ray2,range2] = normc(r-gs3.position_frame);
+    [ray3,range3] = normc(r-gs2.position_frame);
+    
+    % Calculate the doppler measurements:
+    v_rel = v - cross(earth_angularRate,gs1.position_frame);
+    range_rate = scalarProjection(v_rel,ray1);
+    doppler1 = (1 + range_rate/299792458)*gs1.frequency;
+    
+    v_rel = v - cross(earth_angularRate,gs2.position_frame);
+    range_rate = scalarProjection(v_rel,ray2);
+    doppler2 = (1 + range_rate/299792458)*gs2.frequency;
+    
+    v_rel = v - cross(earth_angularRate,gs3.position_frame);
+    range_rate = scalarProjection(v_rel,ray3);
+    doppler3 = (1 + range_rate/299792458)*gs3.frequency;
+
+    
+    % Format the predicted measurements:
+    predicted_measurement = [range1;range2;range3;doppler1;doppler2;doppler3];
+    
+    % Only return the measurements that are actually available:
+    predicted_measurement = predicted_measurement(avails);
+end
